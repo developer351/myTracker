d03b740016153d1e49b865c31e95cbb098c9b27c
evgeniy <developer351@gmail.com>
2018-06-25 22:54:31 +0300

    added task entity

---
+++ app/DoctrineMigrations/Version20180625195241.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180625195241 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE task (id INT AUTO_INCREMENT NOT NULL, task_name VARCHAR(255) NOT NULL, task_link VARCHAR(255) NOT NULL, status INT DEFAULT NULL, time INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE task');
+    }
+}
---
+++ src/TrackerBundle/Entity/Task.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace TrackerBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Task
+ *
+ * @ORM\Table(name="task")
+ * @ORM\Entity(repositoryClass="TrackerBundle\Repository\TaskRepository")
+ */
+class Task
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="task_name", type="string", length=255)
+     */
+    private $taskName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="task_link", type="string", length=255)
+     */
+    private $taskLink;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="status", type="integer", nullable=true)
+     */
+    private $status;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="time", type="integer")
+     */
+    private $time;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set taskName
+     *
+     * @param string $taskName
+     *
+     * @return Task
+     */
+    public function setTaskName($taskName)
+    {
+        $this->taskName = $taskName;
+
+        return $this;
+    }
+
+    /**
+     * Get taskName
+     *
+     * @return string
+     */
+    public function getTaskName()
+    {
+        return $this->taskName;
+    }
+
+    /**
+     * Set taskLink
+     *
+     * @param string $taskLink
+     *
+     * @return Task
+     */
+    public function setTaskLink($taskLink)
+    {
+        $this->taskLink = $taskLink;
+
+        return $this;
+    }
+
+    /**
+     * Get taskLink
+     *
+     * @return string
+     */
+    public function getTaskLink()
+    {
+        return $this->taskLink;
+    }
+
+    /**
+     * Set status
+     *
+     * @param integer $status
+     *
+     * @return Task
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return int
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set time
+     *
+     * @param integer $time
+     *
+     * @return Task
+     */
+    public function setTime($time)
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    /**
+     * Get time
+     *
+     * @return int
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+}
+
